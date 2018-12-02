09c187e585eb7f1ee1a2c9dadda0b712123fa508
NishioTadashi <NishioTadashi>
2018-12-02 19:57:25 +0900

    Change pi.py

--- pi.py
+++ pi.py
@@ -1,11 +1,14 @@
-i = 1
-pie = 0
+delta = input("Please Input Allowable Error: ")
+delta = float(delta)
+i = 0
+pi = 0
 while True:
-    if i % 2 == 1:
-        if i % 4 == 1:
-            pie += 4 / i
-        elif i % 4 == 3:
-            pie -= 4 / i
-    if i % 1000000 == 0:
-        print(pie)
+    before = pi
+    if i % 2 == 0:
+        pi += 4 / (2 * i + 1) 
+    else:
+        pi -= 4 / (2 * i + 1)
+    if(before - pi < delta and pi - before < delta):
+        break
     i += 1
+print(pi)
